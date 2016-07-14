b6760993c098b45f9bc40016535a63dc348afca5
kunx-edu <kunx-edu@qq.com>
2016-07-14 10:57:23 +0800

    多线程

--- admin.shop.com/Application/Admin/Controller/TestController.class.php
+++ admin.shop.com/Application/Admin/Controller/TestController.class.php
@@ -15,4 +15,32 @@ namespace Admin\Controller;
  */
 class TestController extends \Think\Controller{
     //put your code here
+    public function sendMail() {
+        $addresses = [
+            'tzaerdcb@sharklasers.com','nazayuqm@sharklasers.com','hstqxdti@sharklasers.com'
+        ];
+        $pool = [];
+        $start = microtime(true);
+        foreach($addresses as $address){
+            $obj = new MyMailThread($address,'哟呵,大爷您来了','小翠,快来招呼雷少爷');
+            $pool[] = $obj;
+            $obj->start();
+        }
+        $end = microtime(true);
+        echo '共耗时' . ($end-$start) . ' s';
+        
+    }
+}
+
+class MyMailThread extends \Thread{
+    private $email,$subject,$content;
+    
+    public function __construct($email, $subject, $content) {
+        $this->email = $email;
+        $this->subject = $subject;
+        $this->content = $content;
+    }
+    public function run() {
+        sendMail($this->email, $this->subject, $this->content);
+    }
 }
